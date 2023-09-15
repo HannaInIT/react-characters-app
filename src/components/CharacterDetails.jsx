6f450f500bdfbdcea12746b2b0b5d50d3233f8f7
HannaInIT <ann10062016@gmail.com>
2023-09-15 10:26:57 +0200

    extrave API URL to env

--- src/components/CharacterDetails.jsx
+++ src/components/CharacterDetails.jsx
@@ -8,8 +8,11 @@ function CharacterDetails() {
 
     const [characterDetails, setCharacterDetails] = useState({});
 
+
+    
+
     useEffect(() => {
-        axios.get("https://ih-crud-api.herokuapp.com/characters/" + characterId)
+        axios.get(`${import.meta.env.VITE_API_URL}/characters/${characterId}`)
             .then( response => {
                 setCharacterDetails(response.data)
             })
